06543580fcc7082e54d70a44dd11ae8e3dcecf08
Irving Juarez <irvingjuarez274@gmail.com>
2026-03-16 20:50:27 -0600

    Fixing tests

--- app/main.py
+++ app/main.py
@@ -2,30 +2,13 @@ from fastapi import FastAPI, HTTPException
 from typing import List
 from .models import Item
 
-from opentelemetry import trace
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
+from azure.monitor.opentelemetry import configure_azure_monitor
 import os
 
+if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+    configure_azure_monitor()
+
 app = FastAPI()
-
-connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
-
-trace.set_tracer_provider(TracerProvider())
-tracer_provider = trace.get_tracer_provider()
-
-exporter = AzureMonitorTraceExporter(
-    connection_string=connection_string
-)
-
-span_processor = BatchSpanProcessor(exporter)
-tracer_provider.add_span_processor(span_processor)
-
-FastAPIInstrumentor.instrument_app(app)
-
 
 @app.get("/")
 def root():
